7de3ec131458a9fae40d64471dd51e32210d98fc
Alfredo Fernandez Carlos <alfredofernandezcarlos@gmail.com>
2021-10-23 08:26:41 -0400

    13 Next config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
+/** 
+ * @type {import('next').NextConfig} 
+ **/
 module.exports = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US",
+  },
+};
+
+console.log('next.config.js', JSON.stringify(module.exports, null, 2))
