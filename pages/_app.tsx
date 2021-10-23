224c29cfe412feda26819898c62edc7023a082e0
Alfredo Fernandez Carlos <alfredofernandezcarlos@gmail.com>
2021-10-23 19:57:24 -0400

    22 Noop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { AppProps } from "next/app"
 import { FC } from "react"
 
+const Noop: FC = ({children }) => <>{children}</>
+
 
 function MyApp({Component, pageProps}: AppProps &  {Component: {Layout: FC}}) {
 
-  const Layout = Component.Layout
+  const Layout = Component.Layout ?? Noop
 
   return (
     <Layout>
