1e90619ad97f52b6dbdee74b852fba2b8371b7bf
Alfredo Fernandez Carlos <alfredofernandezcarlos@gmail.com>
2021-11-08 14:05:46 -0400

    53 Navbar

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 
 import { FC } from "react"
 import s from './Layout.module.css'
-import { Footer } from "@components/common"
+import { Footer, Navbar } from "@components/common"
 
 
 const Layout: FC = ({children}) => {
   return (
     <div className={s.root}>
+      <Navbar />
       <main className="fit">
         { children }
       </main>
